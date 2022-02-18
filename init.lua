dea9b0015db638de8b8fc035d5280aa9dedb124c
David Wildasin <dragonmaus@posteo.net>
2022-02-18 13:46:41 -0500

    Make title more informative. Modelled after neovim's title function, minus any dynamic state indicators.

--- init.lua
+++ init.lua
@@ -1,13 +1,23 @@
 require('vis')
 
 local function set_title(title)
-	vis:command(string.format(":!printf '\\033]0;%s\\007'", title))
+	vis:command(string.format(":!printf '\\033]0;%s - vis\\007'", title))
+end
+
+local function format_path(path)
+	if not path then
+		return nil
+	end
+	path, name = path:match("^(.*/)([^/]+)$")
+	path = path:gsub("^" .. os.getenv("HOME") .. "/", "~/")
+	path = path:gsub("/$", "")
+	return string.format("%s (%s)", name, path)
 end
 
 vis.events.subscribe(vis.events.WIN_OPEN, function(win)
-	set_title(win.file.name or '[No Name]')
+	set_title(format_path(win.file.path) or "[No Name]")
 end)
 
-vis.events.subscribe(vis.events.FILE_SAVE_POST, function(file, path)
-	set_title(file.name)
+vis.events.subscribe(vis.events.FILE_SAVE_POST, function(file, _)
+	set_title(format_path(file.path))
 end)
